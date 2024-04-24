f7e5af06d88c9ff6e088bbd1128e33340b4983cd
Manar alsa <102485093+manarals@users.noreply.github.com>
2024-04-24 16:53:01 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_cv2 import cv2
+from PIL import Image
 import numpy as np
 from keras.models import load_model
 
@@ -10,28 +10,15 @@ def preprocess_image(image):
     image_size = 224
 
     # Resize the image to match the input shape of the model
-    resized_image = cv2.resize(image, (image_size, image_size))
+    resized_image = image.resize((image_size, image_size))
 
-    # Convert the image to LAB color space
-    lab_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2LAB)
+    # Convert the image to array
+    image_array = np.array(resized_image)
 
-    # Apply CLAHE (Contrast Limited Adaptive Histogram Equalization) to the L channel
-    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
-    lab_image[:, :, 0] = clahe.apply(lab_image[:, :, 0])
+    # Normalize pixel values to range [0, 1]
+    image_array = image_array / 255.0
 
-    # Convert LAB image back to BGR
-    final_image = cv2.cvtColor(lab_image, cv2.COLOR_LAB2BGR)
-
-    # Extract the green channel
-    green_channel = final_image[:, :, 1]
-
-    # Apply Gaussian Blur to the green channel
-    blurred_image = cv2.GaussianBlur(green_channel, (3, 3), 0)
-
-    # Stack the blurred green channel to form a 3-channel image
-    processed_image = np.stack([blurred_image] * 3, axis=-1)
-
-    return processed_image
+    return image_array
 
 # Function to make predictions
 def predict_image(image):
@@ -53,14 +40,14 @@ def predict_image(image):
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Read the uploaded image
-    image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+    # Read the uploaded image as PIL Image
+    pil_image = Image.open(uploaded_file)
 
     # Display the uploaded image
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(pil_image, caption='Uploaded Image', use_column_width=True)
 
     # Make prediction
-    prediction = predict_image(image)
+    prediction = predict_image(pil_image)
 
     # Display the predicted probabilities for each class
     class_names = ['Class 0', 'Class 1', 'Class 2', 'Class 3', 'Class 4']  # Modify according to your class names
